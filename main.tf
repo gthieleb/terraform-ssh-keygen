ce173ab690018fcef6608db9e15313e4893bf21c
Gunnar Thielebein <gunnar@thielebein.net>
2024-02-25 13:00:00 +0100

    add variable key type support

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
 locals {
-  ssh_parent_path = pathexpand("~/.ssh")
+  ssh_key_name = var.ssh_key_name != "" ? var.ssh_key_name : "id_rsa_terraform"
+  ssh_parent_path = var.ssh_parent_path != "" ? var.ssh_parent_path : pathexpand("~/.ssh")
+  ssh_public_key_path = join("/", [local.ssh_parent_path, "${var.ssh_key_name}.pub"])
+  ssh_private_key_path = join("/", [local.ssh_parent_path, "${var.ssh_key_name}"])
+
+  rsa_bits = var.key_type != "RSA" ? null : var.rsa_bits
+
 }
 
 resource "tls_private_key" "generated" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
+  algorithm = var.key_type
+  rsa_bits  = local.rsa_bits
 }
 
 resource local_file private_key_file { 
@@ -14,25 +20,35 @@ resource local_file private_key_file {
   content = tls_private_key.generated.private_key_pem
   directory_permission = "0700"
   file_permission = "0600"
-  filename = join("/", [local.ssh_parent_path, "id_rsa_${var.key_name}"])
+  filename = local.ssh_private_key_path
 }
 
-resource local_file public_key_file { 
+resource local_file public_key_file_openssh { 
 
   count = var.create_local_file ? 1 : 0
 
-  content = tls_private_key.generated.public_key_pem
+  content = tls_private_key.generated.public_key_openssh
   directory_permission = "0700"
   file_permission = "0600"
-  filename = join("/", [local.ssh_parent_path,"authorized_keys_${var.key_name}"])
+  filename = local.ssh_public_key_path
 }
 
-output "private_key" {
+output "ssh_private_key" {
   value     = tls_private_key.generated.private_key_pem
   sensitive = true
 }
 
-output "public_key" {
-  value     = tls_private_key.generated.private_key_pem
+output "ssh_public_key" {
+  value     = tls_private_key.generated.public_key_openssh
   sensitive = true
 }
+
+output "ssh_private_key_path" {
+  value = local.ssh_private_key_path
+}
+
+output "ssh_public_key_path" {
+  value = local.ssh_public_key_path
+}
+
+
--- variables.tf
+++ variables.tf
@@ -2,6 +2,17 @@ variable  key_name {
   type = string
 }
 
+variable  key_type {
+  type = string
+  description = "Currently-supported values are: RSA, ECDSA, ED25519"
+  default = "RSA"
+}
+
+variable rsa_bits {
+  type = number
+  default = 4096
+}
+
 variable create_local_file {
   type = bool
   default = false
