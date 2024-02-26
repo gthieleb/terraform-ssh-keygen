b5c28d5e24c15ab5e0f55376c38dfb40dcc1332f
Gunnar Thielebein <gunnar@thielebein.net>
2024-02-26 21:22:41 +0100

    fix file/output openssh

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource local_file private_key_file {
 
   count = var.create_local_file ? 1 : 0
 
-  content = tls_private_key.generated.private_key_pem
+  content = tls_private_key.generated.private_key_openssh
   directory_permission = "0700"
   file_permission = "0600"
   filename = local.ssh_private_key_path
@@ -34,7 +34,7 @@ resource local_file public_key_file_openssh {
 }
 
 output "ssh_private_key" {
-  value     = tls_private_key.generated.private_key_pem
+  value     = tls_private_key.generated.private_key_openssh
   sensitive = true
 }
 
