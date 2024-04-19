7b7cef20d0db8bf17f9069d793d89b434666f892
Gunnar Thielebein <gunnar@thielebein.net>
2024-04-19 08:59:02 +0200

    some updates

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,19 +1,4 @@
-module example_ed25519_local_saved {
+module example {
   source = "../../"
-  ssh_key_type = "ED25519"
-  create_local_file = true
-}
-
-module example_rsa_not_saved_custom_bitsize {
-  source = "../../"
-  ssh_key_type = "RSA"
-  ssh_rsa_bits = 512
-  create_local_file = false
-}
-
-module example_rsa_custom_name_local_saved {
-  source = "../../"
-  ssh_key_name = "mykeyname"
-  ssh_key_type = "RSA"
   create_local_file = true
 }
