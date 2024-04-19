867ba9097e4410b318045f121a315efae03e06a6
Gunnar Thielebein <gunnar.thielebein@dataport.de>
2024-04-19 08:59:02 +0200

    some updates

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
 locals {
-  ssh_key_name = var.ssh_key_name != "" ? var.ssh_key_name : "id_terraform_${lower(var.ssh_key_type)}"
+
+  ssh_key_sfx = "id_${lower(var.ssh_key_type)}"
+  ssh_key_name = var.ssh_key_name != "" ? "${var.ssh_key_name}-${local.ssh_key_sfx}" : "terraform-${random_integer.key_id.result}-${local.ssh_key_sfx}"
   ssh_parent_path = var.ssh_parent_path != "" ? var.ssh_parent_path : pathexpand("~/.ssh")
   ssh_public_key_path = join("/", [local.ssh_parent_path, "${local.ssh_key_name}.pub"])
   ssh_private_key_path = join("/", [local.ssh_parent_path, local.ssh_key_name])
 
   rsa_bits = var.ssh_key_type != "RSA" ? null : var.ssh_rsa_bits
 
+
+}
+
+resource "random_integer" "key_id" {
+  min = 11111
+  max = 99999
 }
 
 resource "tls_private_key" "generated" {
